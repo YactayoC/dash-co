ef62fe5f6ef770ca6b1509d4a2af69ff13fa2d8d
YactayoC <syactayocrisostomo@gmail.com>
2024-11-16 20:47:25 -0500

    add interceptor

--- src/config/awsAPI.ts
+++ src/config/awsAPI.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "http://3.87.145.137:3000/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.baseURL = config?.baseURL?.replace(/^https?:/, "http:");
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
